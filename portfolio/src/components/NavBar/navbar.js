a6ec39ef0dbddb9da3f280c01305af6aa563d951
T33C33 <teeceeiheukwumere@gmail.com>
2025-01-17 13:07:31 +0100

    navlinks active color

--- portfolio/src/components/NavBar/navbar.js
+++ portfolio/src/components/NavBar/navbar.js
@@ -10,14 +10,60 @@ const Navbar = () => {
     <nav className="navbar">
       <img src={logo} alt="logo" className="logo" />
       <div className="desktopMenu">
-        <Link className="desktopMenuListItem">Home</Link>
-        <Link className="desktopMenuListItem">About</Link>
-        <Link className="desktopMenuListItem">Portfolio</Link>
-        <Link className="desktopMenuListItem">Services</Link>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Portfolio
+        </Link>
+        <Link
+          activeClass="active"
+          to="clients"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Services
+        </Link>
         <Link className="desktopMenuListItem">SEOlympus</Link>
       </div>
-      <button className="desktopMenuBtn">
-        {/* <img src={contactImg} alt="" className="desktopMenuImg" /> */}
+      <button
+        className="desktopMenuBtn"
+        onClick={() => {
+          document
+            .getElementById('contact')
+            .scrollIntoView({ behavior: 'smooth' });
+        }}
+      >
         <MdContactEmergency className="desktopMenuImg" />
         Contact Me
       </button>
